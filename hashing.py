a858f5655eb242ed4651292f678a898e5aa4962a
Mohammed Mujahid <mujahid14916@gmail.com>
2019-01-06 21:42:35 +0530

    Added Search by Key methods

--- hashing.py
+++ hashing.py
@@ -2,7 +2,7 @@ import random
 from time import time
 
 
-random.seed(50)
+# random.seed(50)
 
 
 def is_prime(n):
@@ -12,7 +12,7 @@ def is_prime(n):
     return True
 
 
-def get_prime(start, count=1, step=1, multiplier=True):
+def get_prime(start, count=1, step=1, multiplier=False):
     i = 0
     number = start
     while count > i:
@@ -39,7 +39,7 @@ def linear_insert(table, data):
     size = len(table)
     for val in data:
         n = 0
-        while n < size:
+        while size > n:
             key = (val + n) % size
             if table[key] is None:
                 table[key] = val
@@ -52,6 +52,18 @@ def linear_insert(table, data):
     return table, collision, insert_fail
 
 
+def linear_search(table, element):
+    n = 0
+    size = len(table)
+    while size > n:
+        key = (element + n) % size
+        if table[key] == element:
+            return n+1, key, 'F'
+        else:
+            n += 1
+    return n, -1, 'NF'
+
+
 def quadratic_insert(table, data):
     insert_fail = 0
     collision = []
@@ -59,7 +71,7 @@ def quadratic_insert(table, data):
     stop_size = size//2
     for val in data:
         n = 0
-        while n < stop_size:
+        while stop_size > n:
             key = (val + n*n) % size
             if table[key] is None:
                 table[key] = val
@@ -72,11 +84,23 @@ def quadratic_insert(table, data):
     return table, collision, insert_fail
 
 
+def quadratic_search(table, element):
+    n = 0
+    size = len(table)
+    while size > n:
+        key = (element + n * n) % size
+        if table[key] == element:
+            return n+1, key, 'F'
+        else:
+            n += 1
+    return n, -1, 'NF'
+
+
 def d_hash_insert(table, data):
     insert_fail = 0
     collision = []
     size = len(table)
-    prime = next(get_prime(size, 1, -1, False))
+    prime = next(get_prime(size, 1, -1))
     for val in data:
         n = 0
         key = val % size
@@ -98,6 +122,26 @@ def d_hash_insert(table, data):
     return table, collision, insert_fail
 
 
+def d_hash_search(table, element):
+    size = len(table)
+    n = 0
+    prime = next(get_prime(size, 1, -1))
+
+    key = element % size
+    if table[key] == element:
+        return n+1, key, 'F'
+    else:
+        n = 1
+        offset = prime - element % prime
+        while size > n:
+            key = (key + offset) % size
+            if table[key] == element:
+                return n+1, key, 'F'
+            else:
+                n += 1
+    return n, -1, 'NF'
+
+
 def rehash_insert(table, data):
     collision = []
     size = len(table)
@@ -106,10 +150,12 @@ def rehash_insert(table, data):
     for val in data:
         n = 0
         if i > len(table)//2:
-            table = [None] * next(get_prime(2*len(table), 1, 1, False))
-            _, c, _ = quadratic_insert(table, data[:i])
-            n = sum(c)
-        while n < stop_size:
+            table = [None] * next(get_prime(2*len(table), 1, 1))
+            size = len(table)
+            stop_size = size//2
+            _, collision, _ = quadratic_insert(table, data[:i])
+            # n = sum(c)
+        while stop_size > n:
             key = (val + n*n) % size
             if table[key] is None:
                 table[key] = val
@@ -121,25 +167,99 @@ def rehash_insert(table, data):
     return table, collision, 0
 
 
+def rehash_search(table, element):
+    n = 0
+    size = len(table)
+    while size > n:
+        h = (element + n*n) % size
+        if table[h] == element:
+            return n+1, h, 'F'
+        n += 1
+    return n, -1, 'NF'
+
+
+def cuckoo_insert(table, data):
+    m = 2*len(table)
+    while True:
+        m = 2*m
+        # TODO: Actual Collisions
+        collisions = []
+        insert_failed = False
+        hashed = {}
+        next_prime = next(get_prime(m))
+        for val in data:
+            hashed[val] = [val % m, (val*next_prime) % m]
+
+        table1 = [None] * m
+        table2 = [None] * m
+
+        for val in data:
+            element_to_insert = val
+            count = 0
+            while element_to_insert:
+                first = hashed[element_to_insert][0]
+                if table1[first] is None:
+                    table1[first] = element_to_insert
+                    element_to_insert = None
+                else:
+                    count += 1
+                    element_to_insert, table1[first] = table1[first], element_to_insert
+                    second = hashed[element_to_insert][1]
+                    if table2[second] is None:
+                        table2[second] = element_to_insert
+                        element_to_insert = None
+                    else:
+                        count += 1
+                        element_to_insert, table2[second] = table2[second], element_to_insert
+                if count > m:
+                    insert_failed = True
+                    break
+            if insert_failed:
+                break
+            collisions.append(count)
+        if not insert_failed:
+            break
+    return [table1, table2], collisions, 0
+
+
+def cuckoo_search(table, val):
+    comps = 0
+    table_size = len(table[0])
+    h1 = val % table_size
+    comps += 1
+    if table[0][h1] == val:
+        return comps, h1, 'F'
+    h2 = (val*get_prime(table_size)) % table_size
+    comps += 1
+    if table[1][h2] == val:
+        return comps, h2, 'F'
+    return comps, -1, 'NF'
+
+
 def main():
     hash_methods = {
         'Linear': linear_insert,
         'Quadratic': quadratic_insert,
         'Double Hash': d_hash_insert,
-        'Rehash': rehash_insert
+        'Rehash': rehash_insert,
+        'Cuckoo': cuckoo_insert,
     }
 
     for _ in range(91):
         print('-', end='')
     print()
-    print("{:15s}{:35s}{:25s}{:25s}".format("Table Size",
-                                            "Total Collisions",
-                                            "Total Insert Failed",
-                                            "Total Time Taken"))
+    print('A: Table Size')
+    print('B: Total Collisions')
+    print('C: Total Insert Failed')
+    print('D: Total Time Taken')
+    print("{:13s}{:30s}{:7s}{:15s}".format('{:>10s}'.format("A"),
+                                           '{:>27s}'.format("B"),
+                                           '{:>5s}'.format("C"),
+                                           '{:>12s}'.format("D")))
     for _ in range(91):
         print('-', end='')
     print()
-    for k in get_prime(10, 5, 20):
+    for k in get_prime(10, 5, 20, True):
         data = random.sample(set(range(k * 5)), k)
         for p, (name, method) in enumerate(hash_methods.items()):
             table = [None] * k
@@ -150,10 +270,10 @@ def main():
                 size = ''
             else:
                 size = k
-            print("{:15s}{:35s}{:25s}{:25s}"
+            print("{:13s}{:30s}{:7s}{:15s}"
                   .format('{:>10s}'.format(str(size)),
                           '{:12s}'.format(name) + '{:>15s}'.format(str(sum(collision))),
-                          '{:>10s}'.format(str(insert_fail)),
+                          '{:>5s}'.format(str(insert_fail)),
                           '{:>12s}'.format('{:0.4f}'.format(t) + ' s'))
                   )
         for _ in range(91):
